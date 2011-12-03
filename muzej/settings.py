3378f49adfcdf0031dfdfe906af05d22ac2747a1
Tomaz Solc <tomaz.solc@ijs.si>
2011-12-03 18:52:52 +0100

    Popravil formate datumov

--- muzej/settings.py
+++ muzej/settings.py
@@ -17,6 +17,10 @@ MANAGERS = ADMINS
 # system time zone.
 TIME_ZONE = 'Europe/Ljubljana'
 
+DATE_FORMAT = 'd. m. Y'
+TIME_FORMAT = 'H:i'
+DATETIME_FORMAT = "d. m. Y H:i"
+
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
@@ -29,7 +33,7 @@ USE_I18N = True
 
 # If you set this to False, Django will not format dates, numbers and
 # calendars according to the current locale
-USE_L10N = True
+USE_L10N = False
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
