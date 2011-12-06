61f1017ead4fc846d0f055e080e889e916d53a1f
Tomaz Solc <tomaz.solc@tablix.org>
2011-12-06 17:35:42 +0100

    404 if object isn't found

--- muzej/inventura/views.py
+++ muzej/inventura/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.http import Http404
 
 from muzej.inventura.models import Vhod, Primerek
 
@@ -13,9 +14,15 @@ def vhod(request, id):
 	return render(request, 'vhod.html', context)
 
 def vhod_short(request, id):
-	vhod = Vhod.objects.get(pk=id)
+	try:
+		vhod = Vhod.objects.get(pk=id)
+	except Vhod.DoesNotExist:
+		raise Http404
 	return redirect(vhod)
 
 def primerek_short(request, id):
-	primerek = Primerek.objects.get(pk=id)
+	try:
+		primerek = Primerek.objects.get(pk=id)
+	except Primerek.DoesNotExist:
+		raise Http404
 	return redirect(primerek)
